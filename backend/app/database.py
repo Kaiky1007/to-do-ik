ad52955cc5321e719af37ec66b7728a64915b315
Kaiky <kaikyxcunha@gmail.com>
2026-06-19 19:47:43 -0300

    database updated

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,15 +1,22 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-# URL de conexão com o banco do Docker
-SQLALCHEMY_DATABASE_URL = "postgresql://tododb:kaikytododb@localhost:5432/todo_db"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
-# Dependency para obter a sessão do banco nas rotas do FastAPI
 def get_db():
     db = SessionLocal()
     try:
